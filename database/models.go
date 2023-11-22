55e676d0a171e00ca38d1c8c3b3e7688a325c509
paerarason <paerarason1111@gmail.com>
2023-11-22 20:34:54 +0530

    gorm

--- database/models.go
+++ database/models.go
@@ -1,12 +1,20 @@
 package database
 
-type User struct { 
-    name string 
-    password string
-    role string 
-    team string
-}
+import (
+    "gorm.io/gorm"
+)
 
+type User struct {
+    ID           uint       `gorm:primarykey`
+    Name         string
+    Email        *string
+    Age          uint8
+    Birthday     *time.Time
+    MemberNumber sql.NullString
+    ActivatedAt  sql.NullTime
+    CreatedAt    time.Time
+    UpdatedAt    time.Time
+  }
 
 type Lists struct {
     task string 
--- stratergy/middleware/authentication.go
+++ stratergy/middleware/authentication.go
@@ -2,79 +2,8 @@ package middleware
 import (
 	"context"
 	"fmt"
-	"net/http"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
-var (
-	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "YOUR_GOOGLE_REDIRECT_URL",
-		ClientID:     "YOUR_GOOGLE_CLIENT_ID",
-		ClientSecret: "YOUR_GOOGLE_CLIENT_SECRET",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
-	}
-)
+func JWTAuthentication(){
 
-func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-	url := googleOauthConfig.AuthCodeURL("state")
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
-
-func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
-	state := r.FormValue("state")
-	if state != "state" {
-		http.Error(w, "Invalid state parameter", http.StatusBadRequest)
-		return
-	}
-
-	code := r.FormValue("code")
-	token, err := googleOauthConfig.Exchange(context.Background(), code)
-	if err != nil {
-		http.Error(w, "Failed to exchange token", http.StatusBadRequest)
-		return
-	}
-
-	// Use the token to get user information from the Google API
-	// ...
-
-	// Redirect or respond as needed
-}
-
-var (
-	githubOauthConfig = &oauth2.Config{
-		RedirectURL:  "YOUR_GITHUB_REDIRECT_URL",
-		ClientID:     "YOUR_GITHUB_CLIENT_ID",
-		ClientSecret: "YOUR_GITHUB_CLIENT_SECRET",
-		Scopes:       []string{"user:email"},
-		Endpoint:     github.Endpoint,
-	}
-)
-
-func handleGitHubLogin(w http.ResponseWriter, r *http.Request) {
-	url := githubOauthConfig.AuthCodeURL("state")
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
-
-func handleGitHubCallback(w http.ResponseWriter, r *http.Request) {
-	state := r.FormValue("state")
-	if state != "state" {
-		http.Error(w, "Invalid state parameter", http.StatusBadRequest)
-		return
-	}
-
-	code := r.FormValue("code")
-	token, err := githubOauthConfig.Exchange(context.Background(), code)
-	if err != nil {
-		http.Error(w, "Failed to exchange token", http.StatusBadRequest)
-		return
-	}
-
-	// Use the token to get user information from the GitHub API
-	// ...
-
-	// Redirect or respond as needed
 }
